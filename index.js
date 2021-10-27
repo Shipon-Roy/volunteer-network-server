7e5e9cc2a48c0fe892022430ca46b87c4a374fcf
Shpon Roy <shiponroy509@gmail.com>
2021-10-28 02:40:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhhgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run () {
+    try{
+        await client.connect();
+        const database = client.db('volunteer_network');
+        const servicesCollection = database.collection('services');
+        const registerCollection = database.collection('register');
+
+        // Get services API 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //Post Register API
+        app.post('/register', async (req, res) => {
+            const register = req.body;
+            const submit = await registerCollection.insertOne(register);
+            res.json(submit);
+        })
+
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Volunteer Network Server Running')
+});
+
+app.listen(port, () => {
+    console.log('Volunteer network at port', port);
+});
